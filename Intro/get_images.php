39c18af3e84afb41d180d012726d04aa82d7ba38
Peter Marcelis <Petermarcelisrocmn@gmail.com>
2024-02-22 10:51:59 +0100

    Adjusted basic get request to make it dynamic.

--- Intro/get_images.php
+++ Intro/get_images.php
@@ -1,4 +1,5 @@
 <?php
+/*
 // Generate static images HTML content
 $imagesHtml = '<img src="/images/pancake1.jpg" alt="Image 1">';
 $imagesHtml .= '<img src="images/pancake2.jpg" alt="Image 2">';
@@ -11,6 +12,24 @@ $imagesHtml .= '<img src="images/pancake2.jpg" alt="Image 2">';
 $imagesHtml .= '<img src="images/pancake1.jpg" alt="Image 3">';
 // Send the HTML content back to the client
 echo $imagesHtml;
-?>
+*/
 
+// Path to the images folder
+$imagesPath = 'images/';
 
+// Get all image filenames from the images folder
+$imageFiles = glob($imagesPath . '*.{jpg,jpeg,png,gif}', GLOB_BRACE);
+
+// Initialize the HTML content
+$imagesHtml = '';
+
+// Loop through each image filename and generate HTML content
+foreach ($imageFiles as $imageFile) {
+    // Get the image filename without the path
+    $imageName = basename($imageFile);
+    // Generate HTML for the image
+    $imagesHtml .= '<img src="' . $imagesPath . $imageName . '" alt="' . $imageName . '">';
+}
+
+// Send the HTML content back to the client
+echo $imagesHtml;
