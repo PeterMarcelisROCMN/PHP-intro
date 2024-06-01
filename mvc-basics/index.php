a3e09a5c8de6f14e6c087d7abfec170dcc902718
Peter Marcelis <Petermarcelisrocmn@gmail.com>
2024-06-01 18:43:32 +0200

    basic url rewriting (minor adjustment because it's in a subfolder now has pretty url's.

--- mvc-basics/index.php
+++ mvc-basics/index.php
@@ -1,8 +1,23 @@
 <?php
 
-// get the action and the controller from the URL
-$action = $_GET['action'];
-$controller = $_GET['controller'];
+// Get the values for the request uri and the original script name
+$request_uri = $_SERVER['REQUEST_URI'];
+$script_name = $_SERVER['SCRIPT_NAME'];
+
+// Strip the base path from the request URI
+$base_path = str_replace('/index.php', '', $script_name);
+$relative_uri = substr($request_uri, strlen($base_path));
+
+// Split the relative URI into parts 
+// /home/index?name=Pete will exclude the ?name=Pete part
+$path = parse_url($relative_uri, PHP_URL_PATH);
+
+$segments = explode('/', $path);
+// print_r($segments);
+
+// Get the controller and action
+$controller = $segments[1] ?? 'home';
+$action = $segments[2] ?? 'index';
 
 // dynamically load the controller
 require 'src/controllers/' . $controller . '.php';
